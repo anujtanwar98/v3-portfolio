79e4944615b64e2e9d0440750349d31abbd79afd
Anuj <anujtanwar89@gmail.com>
2023-02-21 00:40:45 -0500

    Update .env.local.php

--- .env.local.php
+++ .env.local.php
@@ -2,10 +2,10 @@
 // Specific to the current environment you're on.
 $APP_CONFIG = [
     'environment' => 'development',
-    'site_name' => 'Tyson Bees',
+    'site_name' => 'V3 Portfolio',
     'site_url' => 'http://localhost/v3-portfolio',
     'database_host' => 'localhost',
     'database_user' => 'root',
     'database_pass' => '',
-    'database_name' => 'local_idm216',
+    'database_name' => '',
 ];
